6a59afcb02a7eaa335efbaace2c34347a9fd51a6
Ed Esquivel <edgaresquivel0615@gmail.com>
2025-01-31 00:13:00 -0500

    Complete Header component and add Routing

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,23 @@
 import React from "react";
-import logo from "../icons_assets/Logo.svg";
-function Header() {
+import { Link } from "react-router-dom";
+import bannerImg from "../icons_assets/restauranfood.jpg"
+
+const Header = () => {
   return (
     <>
-      <header>
-        <figure>
-          <img src={logo} alt="Website Logo" />
-        </figure>
+      <header className="header">
+        <section>
+          <div className="banner">
+            <h2>Little Lemon</h2>
+            <h3>Chicago</h3>
+            <p>We are a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist.</p>
+            <Link to="/booking"><button aria-label="On Click">Reserve a table</button></Link>
+          </div>
+
+          <div className="banner-img">
+            <img src={bannerImg} alt=""/>
+          </div>
+        </section>
       </header>
     </>
   );
